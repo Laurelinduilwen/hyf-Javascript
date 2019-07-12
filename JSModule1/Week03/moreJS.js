33708ea97464c6636cb33231c6d8c202748a1a35
Laurelinduilwen <ladylaurelin@hotmail.com>
2019-07-12 03:03:11 +0200

    implementing feedback for week03

--- JSModule1/Week03/moreJS.js
+++ JSModule1/Week03/moreJS.js
@@ -1,11 +1,11 @@
 //part 1
-console.log("----------------------Part 1----------------------");
+console.log('----------------------Part 1----------------------');
 
-function part1(x, y, z) {
-  console.log((i = (x + y + z) / 3));
+function sumThemAll(x, y, z) {
+  console.log((i = x + y + z));
 }
 
-part1(345, 18345, 6756);
+sumThemAll(345, 18345, 6756);
 
 // ES6
 
@@ -16,204 +16,217 @@ const part1a = (x, y, z) => {
 console.log(part1a(345, 18345, 6756));
 
 //part 2
-console.log("----------------------Part 2----------------------");
+console.log('----------------------Part 2----------------------');
 
-const colorCar = x => {
-  return "a " + x + " car";
+const colorCar = color => {
+  return 'a ' + color + ' car';
 };
 
-console.log(colorCar("red"));
+console.log(colorCar('red'));
 
 //part 3
-console.log("----------------------Part 3----------------------");
+console.log('----------------------Part 3----------------------');
 
 let myComputer = new Object();
 myComputer.brand = 'Asus';
 myComputer.cpu = 'Intel Core i7-8750H';
-myComputer.gpu = 'NVIDIA GeForce GTX 1060' ;
+myComputer.gpu = 'NVIDIA GeForce GTX 1060';
 myComputer.nvme = 'Samsung 970 EVO Plus 1TB';
 myComputer.summary = 'PC Gaming Mustard Race';
 
 function showProps(obj, objName) {
   var result = ``;
   for (var i in obj) {
-    if (obj.hasOwnProperty(i)) {
-      result += `${objName}.${i} = ${obj[i]}\n`;
-    }
+    obj.hasOwnProperty(i);
+    result += `${objName}.${i} = ${obj[i]}\n`;
   }
   return result;
 }
 
-console.log(showProps(myComputer, "myComputer"));
+console.log(showProps(myComputer, 'myComputer'));
 
 //part 4
-console.log("----------------------Part 4----------------------");
+console.log('----------------------Part 4----------------------');
 
-const vehicleType = (x, y) => {
+const vehicleType = (color, type) => {
   var y;
-  if (y === 1) {
-    y = "car";
-  } else if (y === 2) {
-    y = "motorbike";
+  if (type === 1) {
+    type = 'car';
+  } else if (type === 2) {
+    type = 'motorbike';
   } else {
-    y = "teapot";
+    type = 'teapot';
   }
-  return "a " + x + " " + y;
+  return 'a ' + color + ' ' + type;
 };
 
-console.log(vehicleType("blue", 2));
+console.log(vehicleType('blue', 2));
 
 //part 5
-console.log("----------------------Part 5----------------------");
+console.log('----------------------Part 5----------------------');
 
-console.log(3 === 3 ? "yes" : "no");
+console.log(3 === 3 ? 'yes' : 'no');
 
 //part 6
-console.log("----------------------Part 6----------------------");
+console.log('----------------------Part 6----------------------');
 
 let vehicle = (x, y, z) => {
   var y;
   if (y === 1) {
-    y = "car";
+    y = 'car';
   } else if (y === 2) {
-    y = "motorbike";
+    y = 'motorbike';
   } else {
-    y = "teapot";
+    y = 'teapot';
   }
 
   if (z >= 1) {
-    z = "used";
+    z = 'used';
   } else {
-    z = "new";
+    z = 'new';
   }
-  return "a " + x + " " + z + " " + y;
+  return 'a ' + x + ' ' + z + ' ' + y;
 };
 
-console.log(vehicle("blue", 1, 5));
+console.log(vehicle('blue', 1, 5));
 
 //part 7 & 8
-console.log("----------------------Part 7 & 8----------------------");
+console.log('----------------------Part 7 & 8----------------------');
 
-const listOfVehicles = ["motorbike", "caravan", "spaceship", "bike"];
+const listOfVehicles = ['motorbike', 'caravan', 'spaceship', 'bike'];
 
 console.log(listOfVehicles[2]);
 
 //part 9
-console.log("----------------------Part 9----------------------");
+console.log('----------------------Part 9----------------------');
 
 vehicle = (x, y, z) => {
   var y = listOfVehicles[y];
 
   if (z >= 2) {
-    z = "used";
+    z = 'used';
   } else {
-    z = "new";
+    z = 'new';
   }
-  return "a " + x + " " + z + " " + y;
+  return 'a ' + x + ' ' + z + ' ' + y;
 };
 
-console.log(vehicle("green", 3, 1)); //
+console.log(vehicle('green', 3, 1)); //
 
 //part 10
-console.log("----------------------Part 10----------------------");
+console.log('----------------------Part 10----------------------');
 
+function resultSentence() {
+  let resultString = ``;
+  for (i = 0; i < listOfVehicles.length - 1; i++) resultString += listOfVehicles[i] + 's, ';
 
-let resultString = "";
+  resultString += 'and ' + listOfVehicles[listOfVehicles.length - 1] + 's.';
 
-for (var i = 0; i < listOfVehicles.length-1; i++)
+  return resultString;
+}
 
-resultString += listOfVehicles[i] + "s, ";
-resultString += "and " + listOfVehicles[listOfVehicles.length-1] +"s.";
-
-let amazingJoe = "Amazing Joe's Garage, we service " + resultString;
-
+let amazingJoe = "Amazing Joe's Garage, we service " + resultSentence();
 
 console.log(amazingJoe);
 
 //part 11
-console.log("----------------------Part 11----------------------");
+console.log('----------------------Part 11----------------------');
 
 listOfVehicles.splice(0, 0, 'mecha');
 
-console.log(listOfVehicles); 
+console.log(listOfVehicles);
 
 //we can run the code for the for loop again in here OR do it the dirty way
+//edit- much more efficient with the function now :D
 
-amazingJoe = "Amazing Joe's Garage, we service " + listOfVehicles[0]+"s " + resultString;
+amazingJoe = "Amazing Joe's Garage, we service " + resultSentence();
 
 console.log(amazingJoe);
 
 //part 12
-console.log("----------------------Part 12----------------------");
+console.log('----------------------Part 12----------------------');
 
 const emptyObject = new Object();
 
 console.log(emptyObject);
 
 //part 13
-console.log("----------------------Part 13 & 14----------------------");
+console.log('----------------------Part 13 & 14----------------------');
 
 function Teacher(name, language) {
   this.name = name;
   this.language = language;
 }
 
-const teachers = [(new Teacher('Philipp', 'HTML-CSS')), (new Teacher('Igor', 'CLI & GIT')), (new Teacher('Nick', 'JavaScript'))];
+const teachers = [
+  new Teacher('Philipp', 'HTML-CSS'),
+  new Teacher('Igor', 'CLI & GIT'),
+  new Teacher('Nick', 'JavaScript'),
+];
 
 console.log(teachers);
 
-
 //part 15
-console.log("----------------------Part 15----------------------");
+console.log('----------------------Part 15----------------------');
 
 let x = [1, 2, 3];
 let y = [1, 2, 3];
 let z = y;
 
-
-console.log(x == y);  //false
-console.log(x === y);  //false
-console.log(z == y);  //true
-console.log(z == x);  //false
+console.log(x == y); //false
+console.log(x === y); //false
+console.log(z == y); //true
+console.log(z == x); //false
 
 /* the problem here is the equality operator does not look for the contents of and array when you try to compare two different arrays.
 z and y works because we assigned the variable y and its contents to the variable z.
+
+edit - when we assign an array to another array like this we are simply changing what object the variable refers to.
+in this case when we assign y to z like this we dont create a new copy. 
 */
+y.pop();
+
+//we popped 3 from y
 
 console.log(z);
 
-console.log(x[0,1,2] === y[0,1,2]); //the contents are the same so it even works with the strict equality operator
+//when we console log z we can see that it is actually referencing y
+
+z.push(99);
+
+console.log(y);
+
+//we pushed to z but logged y :D I think this clearly demonstrates the idea.
+
+console.log(x[(0, 1, 2)] === y[(0, 1, 2)]); //the contents are the same so it even works with the strict equality operator
 
 //part 16
-console.log("----------------------Part 16----------------------");
+console.log('----------------------Part 16----------------------');
 
-let o1 = { foo: "bar" };
-let o2 = { foo: "bar" };
+let o1 = { foo: 'bar' };
+let o2 = { foo: 'bar' };
 let o3 = o2;
 
 console.log(o2);
 
 //1- o3 is already assigned so messing with o2 after the assignment wont change anything
 
-o2 = { foo: "something" };
+o2 = { foo: 'something' };
 
 console.log(o2);
 console.log(o3);
 
 //2- o2 = o3 or o3 = o2 does matter because before "o3 = o2" the o3 is undefined
 
+//edit - I also demonstrated the idea in detail in the previous question.
+
 //part 17
-console.log("----------------------Part 17----------------------");
+console.log('----------------------Part 17----------------------');
 
 let bar = 42;
 typeof typeof bar;
 
 console.log(typeof bar);
 
-
 //typeof bar is "number" which happens to be a string since it is a string :D when we test it the return we get back is "string"
-// typeof typeof of anything should return a string because we are asking for a string to describe the type 
-
-
-
-
+// typeof typeof of anything should return a string because we are asking for a string to describe the type
